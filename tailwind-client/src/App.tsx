4e4c18d5271da1079a775c68706705397f1a6040
SayefReyadh <reyadh@craftsmenltd.com>
2022-07-16 15:42:39 +0600

    #1 tailwind video example

--- tailwind-client/src/App.tsx
+++ tailwind-client/src/App.tsx
@@ -1,22 +1,40 @@
-import "./App.css";
+import './App.css';
 
-import React from "react";
+import React from 'react';
+
+import profilePhoto from './photo.jpg';
 
 function App() {
-	const buttonCount = 20;
+  const name = 'Sayef Reyadh';
+  const designation = 'Software Development Engineer I';
+  const company = 'Craftsmen';
+  const author = '- Ada Lovelace';
+  const quote =
+    'We may say most aptly that the Analytical Engine weaves algebraical patterns just as the Jacquard loom weaves flowers and leaves.';
 
-	return (
-		<div className='bg-red-200 w-full h-auto'>
-			{Array.from({ length: buttonCount }).map((_, index) => (
-				<button
-					key={index}
-					className='h-24 w-24 bg-red-700 m-2 rounded-full hover:bg-blue-200 hover:rounded-none font-bold text-lg'
-				>
-					{index}
-				</button>
-			))}
-		</div>
-	);
+  return (
+    <div className='flex justify-center'>
+      <div className='max-w-md py-4 px-8 my-20 bg-gray-200 hover:bg-gray-400 shadow-lg rounded-lg transition ease-in-out delay-150 hover:scale-110 duration-300'>
+        <div className='-mt-16 flex justify-end md:justify-center'>
+          <img
+            className='w-20 h-20 md:w-32 md:h-32 object-cover rounded-full border-2 border-blue-500'
+            src={profilePhoto}
+          ></img>
+        </div>
+        <div className='flex flex-col mt-4'>
+          <p className='text-gray-800 text-3xl font-bold'>{name}</p>
+          <div className='text-orange-600 text-md font-medium md:flex gap-1'>
+            <p>{designation}</p> <p>at {company}</p>
+          </div>
+          <br />
+          <div className='text-black font-bold italic'>
+            <p className='text-sm'>{quote}</p>
+            <p className='text-xs'>{author}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
