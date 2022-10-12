ef27c72cfb7bceb0813b5dfb88be143d10131eba
Sayef Reyadh <reyadh@craftsmenltd.com>
2022-10-12 23:30:46 +0600

    queue created

--- aws-lambda/src/lambda/publisher.ts
+++ aws-lambda/src/lambda/publisher.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, ProxyResult } from 'aws-lambda';
 
-export const handle = async (event: APIGatewayEvent): Promise<ProxyResult> => {
+export const handlePublish = async (event: APIGatewayEvent): Promise<ProxyResult> => {
   return {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
